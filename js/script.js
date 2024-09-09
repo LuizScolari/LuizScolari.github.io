fce4a7da5da74f87a847db53d8e8045c2a637ea2
Luiz Scolari <luizrobertoscolari@outlook.com>
2024-09-09 11:42:45 -0300

    changing the layout

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const texts = ["<>", "Welcome", "to my", "website", "</>"];
+let textIndex = 0;
+let charIndex = 0;
+
+function typeText() {
+    const animatedTextElement = document.getElementById('animated-text');
+    if (charIndex < texts[textIndex].length) {
+        animatedTextElement.innerHTML += texts[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeText, 100);
+    } else {
+        textIndex++; 
+        charIndex = 0; 
+
+        if (textIndex < texts.length) {
+            animatedTextElement.innerHTML += '\n'; 
+            setTimeout(typeText, 500); 
+        } else {
+            setTimeout(() => {
+                animatedTextElement.innerHTML = ''; 
+                textIndex = 0; 
+                setTimeout(typeText, 1000); 
+            }, 2000); 
+        }
+    }
+}
+
+window.onload = typeText; // Inicia a animação quando a página carregar
