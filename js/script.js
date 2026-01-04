8b75f9d6d39bbb04b831c8e0c0fd5028fe72321b
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2026-01-04 12:29:04 -0400

    Update my website

--- js/script.js
+++ js/script.js
@@ -1,52 +1,18 @@
-const texts = ["<> Welcome to my website </>"];
-let textIndex = 0;
-let charIndex = 0;
-
-function typeText() {
-    const animatedTextElement = document.getElementById('animated-text');
-    if (charIndex < texts[textIndex].length) {
-        animatedTextElement.innerHTML += texts[textIndex].charAt(charIndex);
-        charIndex++;
-        setTimeout(typeText, 100);
-    } else {
-        textIndex++; 
-        charIndex = 0; 
-
-        if (textIndex < texts.length) {
-            animatedTextElement.innerHTML += '\n'; 
-            setTimeout(typeText, 500); 
-        } else {
-            setTimeout(() => {
-                animatedTextElement.innerHTML = ''; 
-                textIndex = 0; 
-                setTimeout(typeText, 1000); 
-            }, 2000); 
-        }
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-}
 
-function dark_white_mode(event){
-    let back = document.getElementById('back');
-    let text = document.getElementById('Text');
-    var gitLogo = document.getElementById('gitLogo');
-    var linkedLogo = document.getElementById('linkedLogo');
-
-    if (event.target.id === 'moon') {
-        back.style.backgroundColor = "black";
-        text.style.color = 'white';
-        gitLogo.src = "images/githubLogo/github-mark-white.png"
-        linkedLogo.src = "images/linkedinLogo/In-White-96.png"
-    } else if (event.target.id === 'sun') {
-        back.style.backgroundColor = "white";
-        text.style.color = 'black';
-        gitLogo.src = "images/githubLogo/github-mark.png"
-        linkedLogo.src = "images/linkedinLogo/In-Blue-96.png"
+    tablinks = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 window.onload = () => {
-    typeText();
-
-    document.getElementById("moon").addEventListener("click", dark_white_mode);
-    document.getElementById("sun").addEventListener("click", dark_white_mode);
 };
