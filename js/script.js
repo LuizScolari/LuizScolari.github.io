29b505f6ac9ee0d8f8251fc97de2b12ca996908b
Luiz Roberto Pereira Scolari <luizrobertoscolari@outlook.com>
2026-06-14 10:20:25 -0300

    New Updates

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabcontent.length; i++) {
+function showTab(tabName) {
+    var tabcontent = document.getElementsByClassName("tab-content");
+    for (var i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    tablinks = document.getElementsByClassName("tab-btn");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    var tablinks = document.getElementsByClassName("tab-btn");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
     }
 
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    var tab = document.getElementById(tabName);
+    if (!tab) return false;
+
+    tab.style.display = "block";
+    var btn = document.querySelector('.tab-btn[data-tab="' + tabName + '"]');
+    if (btn) btn.classList.add("active");
+    return true;
 }
 
-window.onload = () => {
-};
+function openTab(evt, tabName) {
+    showTab(tabName);
+    history.replaceState(null, "", "#" + tabName);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    var tabName = window.location.hash.replace("#", "");
+    if (!showTab(tabName)) {
+        showTab("about");
+    }
+});
